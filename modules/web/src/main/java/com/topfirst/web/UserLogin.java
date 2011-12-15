383080a6f3fe0a4ddc7e1511954de29507164fc8
Nikolay Chebotaryov <rododin@gmail.com>
2011-12-15 23:29:15 +0200

    1. Lost stuff has been reverted (commonHeader.xhtml) 2. Refactoring and fixes (UserLogin.java) 3. Some redundant things have been removed (UserController.java, MyBean.java)

--- modules/web/src/main/java/com/topfirst/web/UserLogin.java
+++ modules/web/src/main/java/com/topfirst/web/UserLogin.java
@@ -1,7 +1,9 @@
 package com.topfirst.web;
 
-import java.lang.System;
+import javax.faces.bean.ManagedBean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,55 +12,60 @@ import java.lang.System;
  * Time: 1:56
  * To change this template use File | Settings | File Templates.
  */
+@ManagedBean(name = "UserLogin")
+public class UserLogin
+{
+	final static Logger LOG = LoggerFactory.getLogger(UserLogin.class);
 
-public class UserLogin {
-    // getter / setter ------------------------------------
-    public void setUsername(String username)
-    {
-        this.username=username;
-    };
-    public String getUsername()
-    {
-        return username;
-    };
-    public void setPassword(String password)
-    {
-        this.password=password;
-    };
-    public String getPassword()
-    {
-        return password;
-    };
-    public void setAuthResult(boolean authResult)
-    {
-        this.authResult=authResult;
-    }
-    public boolean getAuthResult()
-    {
-        return authResult;
-    };
+	// getter / setter ------------------------------------
+
+	public void setUsername(String username)
+	{
+		this.username=username;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password=password;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setAuthResult(boolean authResult)
+	{
+		this.authResult=authResult;
+	}
+
+	public boolean getAuthResult()
+	{
+		return authResult;
+	}
+
 // bottom listener ------------------------------------
 
 
-    public void loginBottomPressed()
-    {
-        if((username.equals("q"))&&(password.equals("q")))
-        {
-            authResult=true;
-        }else{
-            authResult=false;
-        };
+	public void loginBottomPressed()
+	{
+		authResult = (username.equals("q")) && (password.equals("q"));
+		LOG.info("user auth reasult - " + authResult);
+	}
 
-        System.out.println("user auth reasult -"+authResult);
+	public void logoutBottomPressed()
+	{
+		authResult=false;
+	}
 
-    };
-    public void logoutBottomPressed()
-    {
-        authResult=false;
-    };
+	// attributes -----------------------------------------
 
-    // attributes -----------------------------------------
-    private String username = "";
-    private String password = "";
-    private boolean authResult=false;
+	private String username = "";
+	private String password = "";
+	private boolean authResult=false;
 }
